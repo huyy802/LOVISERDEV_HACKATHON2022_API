fdede896c122c7cd32eaa8d9e44763ec8c1f7d90
kiet <cnpmcs03@gmail.com>
2022-11-26 16:41:54 +0700

    Add: Route, Controll Challenge

---
+++ src/routers/challenge.router.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+
+import { ChallengeController } from "../controllers/challenge.controller.js";
+
+// router.post("/create", ChallengeController.create);
+
+// router.get("/getAll", ChallengeController.getAll);
+
+// router.get("/get/:id", ChallengeController.get);
+
+// router.put("/update/:id", ChallengeController.update);
+
+// router.delete("/delete/:id", ChallengeController.delete);
+
+
+export default router;
