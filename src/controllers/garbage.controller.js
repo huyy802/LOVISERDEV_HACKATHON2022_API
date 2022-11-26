72fd73bf180471940343b87ba356971f01340ded
kiet <cnpmcs03@gmail.com>
2022-11-26 10:54:21 +0700

    add garbage

---
+++ src/controllers/garbage.controller.js
@@ -0,0 +1,88 @@
+import Garbage from "../models/garbage.js";
+
+export const GarbageController = {
+  //Region get all Garbage
+  getAllGarbage: async (req, res) => {
+    console.log("get all");
+    try {
+      const garbage = await Garbage.find();
+      console.log(garbage);
+      res.status(200).json({
+        success: true,
+        message: garbage,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error when get all Garbage",
+      });
+    }
+  },
+
+  //GET An Garbage
+  getGarbage: async (req, res) => {
+    try {
+      const garbage = await Garbage.findById(req.params.id);
+      res.status(200).json({
+        success: true,
+        message: garbage,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error when get an Garbage",
+      });
+    }
+  },
+
+  //DELETE
+  deleteGarbage: async (req, res) => {
+    try {
+      await Garbage.findByIdAndDelete(req.params.id);
+      res.status(200).json({
+        success: true,
+        message: Garbage,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error when delete Garbage",
+      });
+    }
+  },
+
+  //UPDATE
+  updateGarbage: async (req, res) => {
+    try {
+      const garbage = await Garbage.findById(req.params.id);
+      await garbage.updateOne({ $set: req.body });
+      res.status(200).json("Update Successful!");
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error when update Garbage",
+      });
+    }
+  },
+
+  //End region
+  //Region add new Garbage
+  createGarbage: async (req, res) => {
+    try {
+
+    //   const data = new Garbage({
+    //     url: req.body.url,
+    //     name: req.body.name ,
+    //     price : req.body.price,
+
+    //   });
+  
+      await Garbage.create(req.body);
+      return res.status(200).json({ status: true, message: "Garbage created" });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
+  //End region
+ 
+};
